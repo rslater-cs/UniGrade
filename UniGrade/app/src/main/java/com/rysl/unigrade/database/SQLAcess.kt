e15c1edcee9944c637b0938fbc4c082fe620eeb8
RYSL <32707042+SlaterGitHub@users.noreply.github.com>
2019-11-25 13:22:05 +0000

    added databse and subject

--- UniGrade/app/src/main/java/com/rysl/unigrade/database/SQLAcess.kt
+++ UniGrade/app/src/main/java/com/rysl/unigrade/database/SQLAcess.kt
@@ -1,4 +1,15 @@
 package com.rysl.unigrade.database
 
-class SQLAcess {
+import com.rysl.unigrade.learningTree.Learner
+
+class SQLAcess(private val database: SQLiteHelper) {
+
+    //methods for getting data in database
+
+    fun getLearner(parent: Learner?, table: String): ArrayList<Learner>{
+        val query = "SELECT $table.* FROM " + when(parent){
+            null -> table
+            else -> parent
+        }
+    }
 }
